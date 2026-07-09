990601d054e6bdd496c3f02443c2209288103b97
Chethana45 <chethanasriadusumalli@gmail.com>
2026-07-09 21:56:05 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.69%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -8,17 +8,19 @@ public:
         if(n==1 || n==2)
             return 1;
 
-        vector<int> dp(n+1);
-
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int first = 0;
+        int second = 1;
+        int third = 1;
 
         for(int i=3;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            int current = first + second + third;
+
+            first = second;
+            second = third;
+            third = current;
         }
 
-        return dp[n];
+        return third;
     }
 };
