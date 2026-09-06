f034ba14c608d2e9c15eb4fa67a16c0ed489f753
Chethana45 <chethanasriadusumalli@gmail.com>
2026-09-06 23:30:17 +0530

    Time: 0 ms (100%), Space: 8.2 MB (48.36%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int count=0;
-        while(n>0){
-            if(n%2==1)
-            count++;
-            n/=2;
-        }
-        return count;
+  return __builtin_popcount(n);
 
     }
 };
